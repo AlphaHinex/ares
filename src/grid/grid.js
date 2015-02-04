4926879f920e7f9ea2d5750c1afdc4f61e98d4de
AlphaHinex <AlphaHinex@gmail.com>
2015-02-04 11:06:57 +0800

    resize and move columns

--- src/grid/grid.js
+++ src/grid/grid.js
@@ -1,32 +1,46 @@
 'use strict';
 
-angular.module('ares.grid', ['ui.grid', 'ui.grid.pagination', 'ui.grid.pinning'])
+var deps = [
+  'ui.grid', 
+  'ui.grid.pagination', 
+  'ui.grid.pinning',
+  'ui.grid.moveColumns',
+  'ui.grid.resizeColumns'
+];
 
-.directive('aresGrid', ['i18nService', function(service){
-    // Runs during compile
-    return {
-        restrict: 'E',
-        // name: '',
-        // priority: 1,
-        // terminal: true,
-        // scope: {}, // {} = isolate, true = child, false/undefined = no change
-        // controller: function($scope, $element, $attrs, $transclude) {},
-        // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
-        // template: '',
-        // templateUrl: '',
-        // replace: true,
-        // transclude: true,
-        compile: function($tElement, $tAttrs) {
-            var elementHtml = '<div ui-grid="gridOptions" ui-grid-pagination ui-grid-pinning></div>';
-            $tElement.html(elementHtml);
+angular.module('ares.grid', deps)
 
-            return function($scope, $element, $attrs){
-                service.setCurrentLang('zh-cn');
+.directive('aresGrid', ['i18nService', function(service) {
+  // Runs during compile
+  return {
+    restrict: 'E',
+    // name: '',
+    // priority: 1,
+    // terminal: true,
+    // scope: {}, // {} = isolate, true = child, false/undefined = no change
+    // controller: function($scope, $element, $attrs, $transclude) {},
+    // require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+    // template: '',
+    // templateUrl: '',
+    // replace: true,
+    // transclude: true,
+    compile: function($tElement, $tAttrs) {
+      var elementHtml = '<div ui-grid="gridOptions" ' +
+                             'ui-grid-pagination ' + 
+                             'ui-grid-pinning ' + 
+                             'ui-grid-move-columns ' + 
+                             'ui-grid-resize-columns ' + 
+                             '>' + 
+                        '</div>';
+      $tElement.html(elementHtml);
 
-                $scope.gridOptions = $scope.gridOptions || {};
-                $scope.gridOptions.paginationPageSizes = [25, 50, 75];
-                $scope.gridOptions.paginationPageSize = 25;
-            }
-        }
-    };
+      return function($scope, $element, $attrs) {
+        service.setCurrentLang('zh-cn');
+
+        $scope.gridOptions = $scope.gridOptions || {};
+        $scope.gridOptions.paginationPageSizes = [25, 50, 75, 100];
+        $scope.gridOptions.paginationPageSize = 25;
+      }
+    }
+  };
 }]);
