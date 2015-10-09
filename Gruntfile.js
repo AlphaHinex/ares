a2deedab2574f4996714a4d09f3bebc6ed39f619
AlphaHinex <AlphaHinex@gmail.com>
2015-10-10 00:30:40 +0800

    Use jit-grunt instead of load-grunt-tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,11 @@
 
 module.exports = function(grunt) {
 
-    // Load grunt tasks automatically
-    require('load-grunt-tasks')(grunt);
-
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt);
 
     // Configurable paths for the application
     var appConfig = {
